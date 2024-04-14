758fd913d11fc31b6b56885139458218c8cad896
NicolasSocquetJuglard <socquetjuglardnicolas@gmail.com>
2024-04-15 00:51:20 +0200

    recadrage de la pu**

--- Assets/Scripts/LoadEventCard.cs
+++ Assets/Scripts/LoadEventCard.cs
@@ -3,14 +3,16 @@ using UnityEngine.UI;
 using Random = UnityEngine.Random;
 using Image = UnityEngine.UI.Image;
 using JetBrains.Annotations;
+using System;
 
 public class LoadEventCard : MonoBehaviour
 {
-    public GameObject event1Button;
+    
     // Start is called before the first frame update
     void Start()
     {
-        LoadEvent1();
+        String nameTemplate = "Events/Ev{0}";
+        LoadEvent1(nameTemplate);
     }
 
     // Update is called once per frame
@@ -19,17 +21,22 @@ public class LoadEventCard : MonoBehaviour
         
     }
 
-    public void LoadEvent1(){
-        Sprite event1Image;
-        event1Button = GameObject.Find("event1");
-        Debug.Log(event1Button.name + "Bouton event1 chargé");
-        String nameTemplate = "Events/Ev{0}";
+    public void LoadEvent1(String nameTemplate){
+        GameObject event1Button = GameObject.Find("event1");
         int eventNumber = Random.Range(1, 4);
         String event1Name = String.Format(nameTemplate, eventNumber);
-        Debug.Log(event1Name);
-        event1Image = Resources.Load<Sprite>(event1Name);
-        Debug.Log(event1Image.name + "Evènement chargé");
+        Sprite event1Image = Resources.Load<Sprite>(event1Name);
         Image event1ButtonImage = event1Button.GetComponent<Image>();
         event1ButtonImage.sprite = event1Image;
     }
+
+    public void LoadEvent2(String nameTemplate){
+        GameObject event2Button = GameObject.Find("event2");
+        int eventNumber = Random.Range(4, 7);
+        String event2Name = String.Format(nameTemplate, eventNumber);
+        Sprite event2Image = Resources.Load<Sprite>(event2Name);
+        Image event2ButtonImage = event2Button.GetComponent<Image>();
+        event2ButtonImage.sprite = event2Image;
+    }
+
 }
